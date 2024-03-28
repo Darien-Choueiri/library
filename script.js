2f1624baa1698b733ad26ad5dd93e7e94a43aedf
Darien Choueiri <darien.choueiri@gmail.com>
2024-03-28 15:41:22 -0400

    add delete button to remove book from display and storage

--- script.js
+++ script.js
@@ -18,13 +18,41 @@ const display = document.querySelector('.display-lib');
 
 function displayLibrary() {
     display.innerHTML = '';
+    let index = 0;
     myLibrary.forEach(function (book){
         const output = document.createElement('div');
-        output.innerHTML = book.info();
+        const title = document.createElement('div');
+        const author = document.createElement('div');
+        const pages = document.createElement('div');
+        const read = document.createElement('div');
+        const remove = document.createElement('button');
+
         output.classList.add('card');
+
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        pages.innerHTML = book.pages + " pages";
+        read.innerHTML = book.read;
+        remove.innerHTML = "Delete";
+
+        output.dataset.indexNumber = index;
+
+        remove.addEventListener('click', () => {
+           display.removeChild(output);
+           myLibrary.splice(output.dataset.indexNumber, 1);
+        });
+
+        output.appendChild(title); 
+        output.appendChild(author);
+        output.appendChild(pages);
+        output.appendChild(read);
+        output.appendChild(remove);
+
         display.appendChild(output);
+        index += 1;
     });
 }
+
 
 const dialog = document.querySelector('dialog');
 const showButton = document.querySelector('.btn button');
@@ -45,7 +73,7 @@ add.addEventListener('click', (event) => {
     const title = document.querySelector('#title').value;
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('#read').checked;
-    const book = new Book(author, title, pages, read);
+    const book = new Book(title, author, pages, read);
     addBookToLibrary(book);
     event.preventDefault();
     dialog.close();
