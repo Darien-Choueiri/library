fa79a4b223f20caa1c8f34980b9d6c5d623f8b83
Darien Choueiri <darien.choueiri@gmail.com>
2024-04-03 00:53:05 -0400

    change object constructor to a class

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # library
 
-This project involves using object constructors in a project that will store books into a library array. It also allows the user to add and remove books to the library and display them on screen.
+This project involves using (object constructors in an old version I refactored with classes) classes in a project that will store books into a library array. It also allows the user to add and remove books to the library and display them on screen. 
+
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
         return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
     }
 }
